2467b28409ce892bb5afbe5f570c9931a7b9e49f
Marcin Dobrzyński <dobrzynskim.developer@protonmail.com>
2025-12-06 10:27:17 +0100

    refactor: App layout and update assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from './components/ui/button';
+import zusLogo from "./assets/logo_zus_darker_with_text.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-xl h-[100vh] flex flex-col items-center justify-center text-center mx-auto">
+      <div className="w-[250px] mb-[25px]">
+        <img src={zusLogo} alt="Logo ZUS" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+      <h1 className="text-3xl font-bold mb-3">Zakład Ubezpieczeń Społecznych</h1>
+      <p className="mb-[25px]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse dolore fuga aspernatur optio doloribus incidunt!
+      </p>
+      <Button className="cursor-pointer" onClick={() => alert('Test')}>Test button</Button>
+    </div>
   );
 }
 
